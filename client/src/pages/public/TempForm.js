bf706ace47d997a0ade2762cca45708a91224015
Jared Jewitt <jared@freshworks.io>
2020-04-17 12:49:19 -0700

    Adding stepper - WIP

--- client/src/pages/public/TempForm.js
+++ client/src/pages/public/TempForm.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import moment from 'moment';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
+import StepButton from '@material-ui/core/StepButton';
 import StepLabel from '@material-ui/core/StepLabel';
-import moment from 'moment';
+import Button from '@material-ui/core/Button';
 import { Formik, Form as FormikForm } from 'formik';
 
 import { dateToString } from '../../utils';
@@ -16,25 +18,17 @@ import { PrimaryContactInformation } from '../../components/form/PrimaryContactI
 import { SelfIsolationPlan } from '../../components/form/SelfIsolationPlan';
 import { TravelInformation } from '../../components/form/TravelInformation';
 
-function getSteps() {
-  return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
-}
-
 export default () => {
   const [activeStep, setActiveStep] = useState(0);
-  const steps = getSteps();
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
+  const steps = [
+    'Before You Begin',
+    'Contact Information',
+    'Before Workers Arrive',
+    'After Workers Arrive',
+    'If Workers Become Ill',
+    'Review',
+  ];
 
   const formValues = {
 
@@ -77,19 +71,39 @@ export default () => {
     // TODO: ...
   };
 
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStep = (index) => {
+    setActiveStep(index);
+  };
+
   return (
     <Page>
       <Grid item xs={12} sm={11} md={10} lg={8} xl={6}>
 
         {/** Progress */}
         <Box pt={4} pb={4} pl={2} pr={2}>
-          <Stepper activeStep={activeStep} alternativeLabel>
-            {steps.map((label) => (
-              <Step key={label}>
-                <StepLabel>{label}</StepLabel>
-              </Step>
-            ))}
-          </Stepper>
+          <Card>
+            <Stepper
+              alternativeLabel
+              nonLinear
+              activeStep={activeStep}
+            >
+              {steps.map((label, index) => (
+                <Step key={label}>
+                  <StepButton onClick={() => handleStep(index)} completed={false}>
+                    <StepLabel>{label}</StepLabel>
+                  </StepButton>
+                </Step>
+              ))}
+            </Stepper>
+          </Card>
         </Box>
 
         {/** Form */}
@@ -102,14 +116,31 @@ export default () => {
             >
               <FormikForm>
                 <Grid container spacing={2}>
-                  <PrimaryContactInformation />
-                  <TravelInformation />
-                  <SelfIsolationPlan />
-                  <Certify />
+                  {activeStep === 0 && <PrimaryContactInformation />}
+                  {activeStep === 1 && <TravelInformation />}
+                  {activeStep === 2 && <SelfIsolationPlan />}
+                  {activeStep === 3 && <Certify />}
                 </Grid>
               </FormikForm>
             </Formik>
           </Card>
+        </Box>
+
+        {/** Prev / Next */}
+        <Box>
+          <Button
+            disabled={activeStep === 0}
+            onClick={handleBack}
+          >
+            Back
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleNext}
+          >
+            {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+          </Button>
         </Box>
 
       </Grid>
