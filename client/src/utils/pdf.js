4de807d18749548afd73398d802410ea9d58577e
Mauricio Jourdan <mauricio@freshworks.io>
2020-04-29 12:49:38 -0700

    calibrate pdf form

--- client/src/utils/pdf.js
+++ client/src/utils/pdf.js
@@ -5,12 +5,18 @@ export const convertElementToPDF = async (element, fileName) => {
   try {
     const content = [];
     const pageWidth = 950;
-    const pageHeight = 1400;
+    const pageHeight = 1406;
+
+    // html2canvas needs this workaround in order to render svg on Chrome 
+    var svgElements = element.querySelectorAll('svg');
+    svgElements.forEach((item) => {
+      item.setAttribute("width", item.getBoundingClientRect().width);
+      item.style.width = null;
+    });
+
     const childCount = element.getElementsByTagName('*').length
     const elementHeight = childCount * 7.5
     const pageNumber = Math.ceil(elementHeight / pageHeight);
-
-    console.log(pageNumber)
 
     for (let i = 0; i < pageNumber; i++) {
       const canvas = await html2canvas(element, {
@@ -26,6 +32,7 @@ export const convertElementToPDF = async (element, fileName) => {
         width: 500,
       })
     }
+
     if (fileName) {
       pdfMake.createPdf({ content }).download(fileName);
     } else {
