aa880059c723e579c2d0ac7ad6e7eb29d6c21eec
mauriciofreshw <57013645+mauriciofreshw@users.noreply.github.com>
2020-06-08 14:35:15 -0700

    Collection Notice Update (#41)
    * Added copy change
    
    * Single line address

--- client/src/components/form/SectionSix.js
+++ client/src/components/form/SectionSix.js
@@ -193,14 +193,17 @@ export const SectionSix = ({ handleEditClick, isDisabled }) => {
                 Collection Notice
               </Typography>
               <Typography variant="caption" color="textSecondary">
-                Your personal information as well as those of your household is collected by the Ministry of
-                Health under the authority of sections 26(a), (c), (e) and s.27(1)(a)(iii) of the Freedom of
-                Information and Protection of Privacy Act, the Public Health Act and the federal Quarantine Act,
-                for the purposes of reducing the spread of COVID-19. Personal information may be shared with
-                personnel providing support services and follow-up during self-isolation. Should you have any
-                questions or concerns about the collection of your personal information please contact:
+                <br />
+                Your personal information may be collected by the Ministry of Health under the authority of
+                sections 26(a), (c), (e) and indirectly collected by the Ministry of Agriculture under the
+                authority of section 27(1)(a)(iii) of the Freedom of Information and Protection of Privacy
+                Act, the Public Health Act and the federal Quarantine Act, for the purposes of reducing the
+                spread of COVID-19. Should you have any questions or concerns about the collection of your
+                personal information please contact:
                 <br />
                 Title: Ministry of Health, Chief Privacy Officer
+                <br />
+                Address: 3rd floor – 1483 Douglas Street Victoria BC V8W 9P1                
                 <br />
                 Telephone: 236-478-1666
               </Typography>
