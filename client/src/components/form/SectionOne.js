86033347ff35da8e5b8622f57b6950a58991c8f9
Ashton Meuser <ashtonmeuser@users.noreply.github.com>
2020-08-31 12:45:38 -0700

    Update guidance document links (#48)
    * Update guidance document links
    
    * Update packages
    
    * Update guidance document title

--- client/src/components/form/SectionOne.js
+++ client/src/components/form/SectionOne.js
@@ -41,11 +41,11 @@ export const SectionOne = ({ isDisabled }) => {
                 </Typography>
                 <Typography variant="body1">
                   <Link
-                    href="https://www2.gov.bc.ca/assets/gov/health/about-bc-s-health-care-system/office-of-the-provincial-health-officer/covid-19/covid-19-pho-guidance-farms-farm-workers.pdf"
+                    href="http://www.bccdc.ca/Health-Info-Site/Documents/COVID_public_guidance/All-sector-work-camps-guidance.pdf"
                     rel="noreferrer noopenner"
                     target="_blank"
                   >
-                    Protecting BC Farmers and Farm Workers During the COVID-19 Pandemic
+                    Protecting Industrial Camp Workers, Contractors, and Employers Working in the Agricultural, Forestry, and Natural Resource Sectors During the COVID-19 Pandemic
                   </Link>
                 </Typography>
               </Box>
@@ -76,7 +76,7 @@ export const SectionOne = ({ isDisabled }) => {
                     Farm operators can take practical actions to reduce the risk of disease transmission
                     (see section 2, number 7 of the&nbsp;
                     <Link
-                      href="https://www2.gov.bc.ca/assets/gov/health/about-bc-s-health-care-system/office-of-the-provincial-health-officer/covid-19/covid-19-pho-guidance-farms-farm-workers.pdf"
+                      href="http://www.bccdc.ca/Health-Info-Site/Documents/COVID_public_guidance/All-sector-work-camps-guidance.pdf"
                       rel="noreferrer noopener"
                       target="_blank"
                     >
@@ -86,8 +86,7 @@ export const SectionOne = ({ isDisabled }) => {
                   </Typography>
                 </Card>
                 <Typography variant="body1" paragraph>
-                  Have you worked through all the sections in Protecting BC farmers and farm workers
-                  during the COVID-19 pandemic to identify the risks at your farm?
+                  Have you worked through all the sections in Protecting Industrial Camp Workers, Contractors, and Employers Working in the Agricultural, Forestry, and Natural Resource Sectors During the COVID-19 Pandemic to identify the risks at your farm?
                 </Typography>
               </ExpansionPanelDetails>
             </ExpansionPanel>
@@ -112,7 +111,7 @@ export const SectionOne = ({ isDisabled }) => {
                     to increase cleaning and hygiene, and increase physical distancing, this will form your IPC
                     plan for transportation (see section 2, number 7 of the&nbsp;
                     <Link
-                      href="https://www2.gov.bc.ca/assets/gov/health/about-bc-s-health-care-system/office-of-the-provincial-health-officer/covid-19/covid-19-pho-guidance-farms-farm-workers.pdf"
+                      href="http://www.bccdc.ca/Health-Info-Site/Documents/COVID_public_guidance/All-sector-work-camps-guidance.pdf"
                       rel="noreferrer noopener"
                       target="_blank"
                     >
@@ -229,11 +228,11 @@ export const SectionOne = ({ isDisabled }) => {
           <span>
             I have downloaded and read the Province of BC's COVID-19 requirements,&nbsp;
             <Link
-              href="https://www2.gov.bc.ca/assets/gov/health/about-bc-s-health-care-system/office-of-the-provincial-health-officer/covid-19/covid-19-pho-guidance-farms-farm-workers.pdf"
+              href="http://www.bccdc.ca/Health-Info-Site/Documents/COVID_public_guidance/All-sector-work-camps-guidance.pdf"
               rel="noreferrer noopenner"
               target="_blank"
             >
-              Protecting BC Farmers and Farm Workers During the COVID-19 Pandemic.
+              Protecting Industrial Camp Workers, Contractors, and Employers Working in the Agricultural, Forestry, and Natural Resource Sectors During the COVID-19 Pandemic
             </Link>
           </span>
         )}
